ef5a3734e1d682f8c4bf38a62268df6fb10735ed
Anton Karmanov <a.karmanov@inventati.org>
2021-10-12 18:29:20 +0300

    Add Terraform output vars

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "external_ip_address_app" {
+  description = "Assigned instance IPv4 address"
+  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+}
+
+output "app_link" {
+  description = "Application URL"
+  value = "http://${yandex_compute_instance.app.network_interface.0.nat_ip_address}:9292"
+}
